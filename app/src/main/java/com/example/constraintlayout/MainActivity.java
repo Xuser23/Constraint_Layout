45f9cab72d19ad87c1622de038192a913583752d
GreatD <devamahendrar@gmail.com>
2021-03-13 15:10:58 +0700

    button even click

--- app/src/main/java/com/example/constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.constraintlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnsignin;
+    EditText edpassword, edemail;
+    String nama, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnsignin= findViewById(R.id.btnSigIn);
+        edemail = findViewById(R.id.edEmail);
+        edpassword = findViewById(R.id.edPassword);
+
+        btnsignin.OnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                nama = edemail.getText().toString();
+                password = edpassword.getText().toString();
+
+                Toast t = Toast.makeText(getApplicationContext()),
+                "email anda: "+nama+" dan Password anda: "+password+"", Toast.LENGTH_LONG);
+                t.show();
+
+
+            }
+        });
+
     }
 }
